13b864f970106182cfd4a64670e2754b58d2fed5
stan <anastasia.stieb@gmail.com>
2015-11-15 16:22:50 +0100

    hello angularJS

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
-'use strict';
-require('./app.scss');
+"use strict";
+import "./app.scss";
+import "./hello/hello.jade"
 
-import angular from 'angular';
-import angular_route from  'angular-route';
+import angular from "angular";
+import angular_route from "angular-route";
 
-let appModule = angular.module('app', []);
-angular.element(document).ready(() =>
-  angular.bootstrap(document, [appModule.name], {})
-);
+import HelloController from './hello/hello-controller';
+
+angular.module("pika", ["ngRoute"])
+ .controller("HelloController", HelloController)
+ .config(function($routeProvider){
+   $routeProvider
+    .when("/", {
+      templateUrl: "build/hello/hello.html",
+      controller: "HelloController as controller"
+    });
+ });
---
+++ src/hello/hello-controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+class HelloController {
+	constructor(){
+    this.hello = "bla bla bla";
+	}
+}
+
+export default HelloController;
