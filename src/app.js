3a1139ec549f63ce61e5a57c8818fe7440d22f88
stan <anastasia.stieb@gmail.com>
2015-11-23 13:53:26 +0100

    add photo list

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 "use strict";
 import "./app.scss";
-import "./hello/hello.jade"
+import "./hello/hello.jade";
+import "./photos/photos.jade";
 
 import angular from "angular";
 import angular_route from "angular-route";
 
 import HelloController from './hello/hello-controller';
+import PhotosController from "./photos/photos-controller";
 
 angular.module("pika", ["ngRoute"])
  .controller("HelloController", HelloController)
+ .controller("PhotosController", PhotosController)
  .config(function($routeProvider){
    $routeProvider
     .when("/", {
       templateUrl: "build/hello/hello.html",
       controller: "HelloController as controller"
+    })
+    .when("/photos", {
+      templateUrl: "build/photos/photos.html",
+      controller: "PhotosController as controller"
     });
  });
+
---
+++ src/photos/photos-controller.js
@@ -0,0 +1,30 @@
+'use strict';
+
+class PhotosController{
+  constructor(){
+    this.photos = [
+      {
+       'title': 'Photo',
+       'url': 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
+       'description': 'Your photos'
+      },
+      {
+       'title': 'Photo',
+       'url': 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
+       'description': 'Photo description'
+      },
+      {
+       'title': 'Photo',
+       'url': 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
+       'description': 'Photo description'
+      },
+      {
+       'title': 'Photo',
+       'url': 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
+       'description': 'Photo description'
+      }
+    ];
+  }
+}
+
+export default PhotosController;
