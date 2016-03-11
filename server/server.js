892500ca251097d15988e7bf319e9d017e70af27
Anastasia Stieb <anastasia.stieb@gmail.com>
2016-03-11 13:19:49 +0100

    jwt authentication

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express   = require('express');
+const jwt = require('express-jwt');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
@@ -13,7 +14,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+app.use('/api', jwt({ secret: config.tokenSecret }).unless({path: [/^\/api\/session_token/]}));
 app.use('/api', require('./routes/session_tokens'));
+app.use('/api', require('./routes/users'));
 
 app.listen(config.port);
 console.log('Pika running at http://localhost:' + config.port);
