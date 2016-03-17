72656d229625923cd30deda3e64f488f709faf05
Anastasia Stieb <anastasia.stieb@gmail.com>
2016-03-17 10:21:39 +0100

    side menu

--- src/app.js
+++ src/app.js
@@ -6,15 +6,15 @@ import angular from 'angular';
 import angular_route from 'angular-route';
 import angular_material from 'angular-material';
 
-import HelloController from './hello/hello-controller';
+import SideMenuController from './side-menu-controller';
 import PhotosController from './photos/photos-controller';
 import PhotoDetailController from './photos/photo-detail-controller';
 
 angular.module("pika", ['ngRoute', 'ngMaterial'])
- .controller('HelloController', HelloController)
+ .controller('SideMenuController', SideMenuController)
  .controller('PhotosController', PhotosController)
  .controller("PhotoDetailController", PhotoDetailController)
- .config(function($routeProvider){
+ .config(function($routeProvider, $mdThemingProvider){
    $routeProvider
     .when('/photos', {
       template: require('./photos/photos.jade'),
@@ -24,5 +24,8 @@ angular.module("pika", ['ngRoute', 'ngMaterial'])
       template: require('./photos/photo-detail.jade'),
       controller: "PhotoDetailController as controller"
     });
+    $mdThemingProvider.theme('default')
+    .primaryPalette('blue-grey')
+    .accentPalette('cyan');
  });
 
---
+++ src/side-menu-controller.js
@@ -0,0 +1,57 @@
+'use strict';
+
+class SideMenuController{
+  
+  constructor($mdSidenav){
+   this.$mdSidenav = $mdSidenav;
+   this.initMenu();
+   this.initAdminMenu();
+  }
+
+  initMenu(){
+    this.menu = [
+      {
+        link : '',
+        title: 'Photos',
+        icon: 'image'
+      },
+      {
+        link : '',
+        title: 'Albums',
+        icon: 'collections'
+      }
+    ];
+  }
+  
+  initAdminMenu(){
+    this.adminMenu = [
+      {
+        link : '',
+        title: 'Profile',
+        icon: 'account_box'
+      },
+      {
+        link : '',
+        title: 'Trash',
+        icon: 'delete'
+      },
+      {
+        link : '',
+        title: 'Settings',
+        icon: 'settings'
+      }
+    ];
+  }
+  
+  toggle(){
+    this.$mdSidenav('left').toggle();
+  }
+  
+  close(){
+    this.$mdSidenav('left').close();
+  }
+
+
+}
+
+export default SideMenuController;
