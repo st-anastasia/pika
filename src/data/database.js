27b1dcbbb5cbac0bb9cbf01f5ccd335b58f1ebb9
stan <anastasia.stieb@gmail.com>
2016-04-03 15:45:24 +0200

    gallery

--- src/data/database.js
+++ src/data/database.js
@@ -22,6 +22,66 @@ let photos = [
    title: 'Photo 3',
    url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
    description: 'Photo description'
+  },
+  {
+   id: 0, 
+   title: 'Photo',
+   url: 'http://www.kortenkamp.de/sites/bhkortenkamp.buchhandelsweb.de/files/fruehlingserwachen-a2-small.jpg',
+   description: 'Your photos'
+  },
+  {
+   id: 1,
+   title: 'Photo',
+   url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
+   description: 'Photo description'
+  },
+  {
+   id: 0, 
+   title: 'Photo',
+   url: 'http://www.kortenkamp.de/sites/bhkortenkamp.buchhandelsweb.de/files/fruehlingserwachen-a2-small.jpg',
+   description: 'Your photos'
+  },
+  {
+   id: 1,
+   title: 'Photo',
+   url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
+   description: 'Photo description'
+  },
+  {
+   id: 0, 
+   title: 'Photo',
+   url: 'http://www.kortenkamp.de/sites/bhkortenkamp.buchhandelsweb.de/files/fruehlingserwachen-a2-small.jpg',
+   description: 'Your photos'
+  },
+  {
+   id: 1,
+   title: 'Photo',
+   url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
+   description: 'Photo description'
+  },
+  {
+   id: 0, 
+   title: 'Photo',
+   url: 'http://www.kortenkamp.de/sites/bhkortenkamp.buchhandelsweb.de/files/fruehlingserwachen-a2-small.jpg',
+   description: 'Your photos'
+  },
+  {
+   id: 1,
+   title: 'Photo',
+   url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
+   description: 'Photo description'
+  },
+  {
+   id: 0, 
+   title: 'Photo',
+   url: 'http://www.kortenkamp.de/sites/bhkortenkamp.buchhandelsweb.de/files/fruehlingserwachen-a2-small.jpg',
+   description: 'Your photos'
+  },
+  {
+   id: 1,
+   title: 'Photo',
+   url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
+   description: 'Photo description'
   }
 ];
 
