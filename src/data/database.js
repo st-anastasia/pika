0314c1445484a9207481e07b728ed2f71bd0ff89
Anastasia Stieb <anastasia.stieb@gmail.com>
2016-04-04 11:39:49 +0200

    dummy photos

--- src/data/database.js
+++ src/data/database.js
@@ -1,89 +1,440 @@
-let photos = [
+const photos = [
   {
-   id: 0, 
-   title: 'Photo',
-   url: 'http://www.kortenkamp.de/sites/bhkortenkamp.buchhandelsweb.de/files/fruehlingserwachen-a2-small.jpg',
-   description: 'Your photos'
+    "fileName": "11454863.jpg",
+    "name": "Ipsum labore deserunt consequatur.",
+    "title": "Quia debitis sequi sapiente possimus facilis."
   },
   {
-   id: 1,
-   title: 'Photo',
-   url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
-   description: 'Photo description'
+    "fileName": "1284946545-omgbhd5.jpg",
+    "name": "Velit ducimus fuga.",
+    "title": "Illum officiis sint quasi placeat."
   },
   {
-   id: 2,
-   title: 'Photo',
-   url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
-   description: 'Photo description'
+    "fileName": "13112582.jpg",
+    "name": "Rerum laudantium nemo qui.",
+    "title": "Officiis quis ratione odio."
   },
   {
-   id: 3,
-   title: 'Photo 3',
-   url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
-   description: 'Photo description'
+    "fileName": "13121163.jpg",
+    "name": "Molestias nostrum in tempora officiis sequi quod.",
+    "title": "Delectus et molestiae eveniet."
   },
   {
-   id: 0, 
-   title: 'Photo',
-   url: 'http://www.kortenkamp.de/sites/bhkortenkamp.buchhandelsweb.de/files/fruehlingserwachen-a2-small.jpg',
-   description: 'Your photos'
+    "fileName": "168731824-4ab8e0df62-o.jpg",
+    "name": "Officiis minus nemo aliquid.",
+    "title": "Minima fugiat incidunt labore fugit autem."
   },
   {
-   id: 1,
-   title: 'Photo',
-   url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
-   description: 'Photo description'
+    "fileName": "171847862-685a064114-o.jpg",
+    "name": "Aut iusto consectetur sed doloremque.",
+    "title": "Et nesciunt corrupti quas ratione sit consectetur aut enim."
   },
   {
-   id: 0, 
-   title: 'Photo',
-   url: 'http://www.kortenkamp.de/sites/bhkortenkamp.buchhandelsweb.de/files/fruehlingserwachen-a2-small.jpg',
-   description: 'Your photos'
+    "fileName": "184705692-96675bf4ca-o.jpg",
+    "name": "Sunt minus iure facere veniam est et.",
+    "title": "In ut ut nisi quia voluptas et aspernatur."
   },
   {
-   id: 1,
-   title: 'Photo',
-   url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
-   description: 'Photo description'
+    "fileName": "300412891-e4f70681e0-o.jpg",
+    "name": "Consectetur sit animi consequatur autem explicabo alias.",
+    "title": "Voluptatem ducimus consectetur sint molestiae."
   },
   {
-   id: 0, 
-   title: 'Photo',
-   url: 'http://www.kortenkamp.de/sites/bhkortenkamp.buchhandelsweb.de/files/fruehlingserwachen-a2-small.jpg',
-   description: 'Your photos'
+    "fileName": "4089125.jpg",
+    "name": "Assumenda nihil maiores et ducimus inventore.",
+    "title": "Natus voluptate id officiis sed nobis totam officia excepturi."
   },
   {
-   id: 1,
-   title: 'Photo',
-   url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
-   description: 'Photo description'
+    "fileName": "427526663-a631dcea56-o.jpg",
+    "name": "Expedita error non eum minima.",
+    "title": "Consequuntur odit pariatur quia temporibus vero voluptates a."
   },
   {
-   id: 0, 
-   title: 'Photo',
-   url: 'http://www.kortenkamp.de/sites/bhkortenkamp.buchhandelsweb.de/files/fruehlingserwachen-a2-small.jpg',
-   description: 'Your photos'
+    "fileName": "4305481.jpg",
+    "name": "Voluptatibus voluptas rem ratione veniam nostrum qui.",
+    "title": "Officia et sed quia sit hic."
   },
   {
-   id: 1,
-   title: 'Photo',
-   url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
-   description: 'Photo description'
+    "fileName": "4331012.jpg",
+    "name": "Est qui modi eius veritatis voluptas aut.",
+    "title": "Ea quis cum nihil iste."
   },
   {
-   id: 0, 
-   title: 'Photo',
-   url: 'http://www.kortenkamp.de/sites/bhkortenkamp.buchhandelsweb.de/files/fruehlingserwachen-a2-small.jpg',
-   description: 'Your photos'
+    "fileName": "4393923.jpg",
+    "name": "Nihil minima impedit tempore consequuntur aliquid voluptatem.",
+    "title": "Error ut porro perferendis dignissimos dolores quaerat voluptate quae."
   },
   {
-   id: 1,
-   title: 'Photo',
-   url: 'http://fc02.deviantart.net/fs9/i/2006/066/6/6/muscle_Car_by_ArcAngelTyrael.jpg',
-   description: 'Photo description'
+    "fileName": "4415446.jpg",
+    "name": "Voluptate eius consectetur et modi rem.",
+    "title": "Est quis expedita mollitia quasi non numquam suscipit."
+  },
+  {
+    "fileName": "4473376.jpg",
+    "name": "Harum odit.",
+    "title": "Dolor velit totam et in fugit nemo magnam consequatur."
+  },
+  {
+    "fileName": "4631978836-3d54b641ee-b.jpg",
+    "name": "Eius aperiam.",
+    "title": "Eaque molestiae quas unde vel hic."
+  },
+  {
+    "fileName": "479547.jpg",
+    "name": "Tenetur dolores eum sint.",
+    "title": "Et consequatur quam illo qui et quo corporis est."
+  },
+  {
+    "fileName": "509643781-d44765044a-b.jpg",
+    "name": "Corrupti ut aliquid.",
+    "title": "Quia ut qui excepturi maiores sunt."
+  },
+  {
+    "fileName": "5424603.jpg",
+    "name": "Vel eligendi.",
+    "title": "Eveniet reiciendis sed maiores nulla amet magnam praesentium."
+  },
+  {
+    "fileName": "5780860.jpg",
+    "name": "Dolores recusandae.",
+    "title": "Blanditiis sunt eligendi nulla magnam."
+  },
+  {
+    "fileName": "6156866.jpg",
+    "name": "Repudiandae consequuntur.",
+    "title": "Atque autem veniam quam."
+  },
+  {
+    "fileName": "6262919.jpg",
+    "name": "Hic amet praesentium pariatur sunt.",
+    "title": "Necessitatibus soluta nobis culpa dolores impedit quia sint."
+  },
+  {
+    "fileName": "6767480.jpg",
+    "name": "Sequi atque.",
+    "title": "Illo enim odit qui aut rem est."
+  },
+  {
+    "fileName": "7758913.jpg",
+    "name": "Soluta autem.",
+    "title": "Est dolorem incidunt ut et error non rem voluptas."
+  },
+  {
+    "fileName": "7797696.jpg",
+    "name": "Ea minus rerum doloremque.",
+    "title": "Quasi non molestiae mollitia."
+  },
+  {
+    "fileName": "7813779.jpg",
+    "name": "Necessitatibus accusamus provident.",
+    "title": "Fugiat nihil et est et vero sunt aperiam voluptatem."
+  },
+  {
+    "fileName": "7829363.jpg",
+    "name": "Veniam vel et.",
+    "title": "Hic et earum eos nostrum quaerat similique."
+  },
+  {
+    "fileName": "7850173.jpg",
+    "name": "Debitis laborum modi eum illo ad.",
+    "title": "Eveniet aut sit quas ea culpa nostrum."
+  },
+  {
+    "fileName": "8700169.jpg",
+    "name": "Expedita rem illo.",
+    "title": "Quas unde inventore vel sed autem tempora officia."
+  },
+  {
+    "fileName": "8983344.jpg",
+    "name": "Sapiente et dolores.",
+    "title": "Deserunt ut molestiae iusto sint et unde ea."
+  },
+  {
+    "fileName": "901939.jpg",
+    "name": "Sint odit voluptatum voluptas qui eligendi.",
+    "title": "Assumenda nulla amet facilis ab."
+  },
+  {
+    "fileName": "907657.jpg",
+    "name": "Non quasi accusamus eligendi explicabo reprehenderit.",
+    "title": "Non dolorem enim asperiores."
+  },
+  {
+    "fileName": "915470039-7054e38ec6-b.jpg",
+    "name": "Quia impedit sapiente omnis maiores.",
+    "title": "Excepturi in aliquam veniam ipsam rerum."
+  },
+  {
+    "fileName": "abstract-virus-1280x1024.jpg",
+    "name": "Earum quaerat amet aut quibusdam.",
+    "title": "Illum repudiandae qui voluptas."
+  },
+  {
+    "fileName": "bamboo-1280x1024.jpg",
+    "name": "Ea quidem nesciunt.",
+    "title": "Commodi et unde doloremque corporis est suscipit."
+  },
+  {
+    "fileName": "berlin-nightscreen-gold-edition.jpg",
+    "name": "Temporibus laudantium.",
+    "title": "Sunt est quos similique ut quis alias officia."
+  },
+  {
+    "fileName": "bild-1.png",
+    "name": "Fugiat enim dignissimos.",
+    "title": "Illo qui itaque commodi sapiente totam nesciunt consequuntur molestias."
+  },
+  {
+    "fileName": "cimg0902.jpg",
+    "name": "Alias sint sed.",
+    "title": "Quo sed modi et ut ut facilis suscipit."
+  },
+  {
+    "fileName": "cimg0967.jpg",
+    "name": "Rerum aperiam quis omnis optio esse sed.",
+    "title": "Neque praesentium aut nihil consequatur."
+  },
+  {
+    "fileName": "cimg1007.jpg",
+    "name": "Repellat molestias ut at nihil consequuntur dolore.",
+    "title": "Sed consectetur inventore illo in eum quo alias sunt."
+  },
+  {
+    "fileName": "daffodil-01-1440x900.jpg",
+    "name": "Fugiat enim aut et quos.",
+    "title": "Alias odit ea molestiae deserunt sunt iste."
+  },
+  {
+    "fileName": "daffodil-01.jpg",
+    "name": "A asperiores.",
+    "title": "Fuga debitis autem est."
+  },
+  {
+    "fileName": "daffodil-02.jpg",
+    "name": "Quo autem tempore.",
+    "title": "Quia et distinctio est."
+  },
+  {
+    "fileName": "daffodil-03.jpg",
+    "name": "Quia ex.",
+    "title": "Quia cupiditate minima excepturi."
+  },
+  {
+    "fileName": "daffodil-04.jpg",
+    "name": "Pariatur cumque voluptate veritatis.",
+    "title": "Fugit doloribus amet praesentium eveniet quisquam facere dolor explicabo."
+  },
+  {
+    "fileName": "daffodil-05.jpg",
+    "name": "Nam rerum debitis quo.",
+    "title": "Eaque illum non officiis."
+  },
+  {
+    "fileName": "daffodil-06.jpg",
+    "name": "Non harum natus eligendi.",
+    "title": "Quo incidunt qui et porro voluptatum exercitationem."
+  },
+  {
+    "fileName": "daffodil-07.jpg",
+    "name": "Ullam ex.",
+    "title": "Vel voluptatem officiis esse dolor."
+  },
+  {
+    "fileName": "daffodil-08.jpg",
+    "name": "Non assumenda voluptatibus quis officia voluptatem at.",
+    "title": "Et commodi repellat sunt molestiae et ratione."
+  },
+  {
+    "fileName": "daffodil-09.jpg",
+    "name": "Fugit sint ducimus commodi tempora.",
+    "title": "Est quae necessitatibus magnam."
+  },
+  {
+    "fileName": "daffodil-10.jpg",
+    "name": "Dolorem dolores nobis ex.",
+    "title": "Exercitationem libero voluptatem omnis."
+  },
+  {
+    "fileName": "daffodil-11.jpg",
+    "name": "Accusamus provident est omnis eveniet.",
+    "title": "Soluta pariatur possimus minus quibusdam est explicabo aut inventore."
+  },
+  {
+    "fileName": "daffodil-12.jpg",
+    "name": "Sit ut iste cupiditate.",
+    "title": "Sed corporis debitis tempore est reiciendis non nemo."
+  },
+  {
+    "fileName": "daffodil-13.jpg",
+    "name": "Provident velit.",
+    "title": "Facere voluptates qui nulla temporibus maiores eligendi."
+  },
+  {
+    "fileName": "eiffel-tower,-paris,-france.jpg",
+    "name": "Molestiae unde mollitia et ut nam quibusdam.",
+    "title": "Et itaque deserunt esse provident."
+  },
+  {
+    "fileName": "kamille-01.jpg",
+    "name": "Vero dolorum dolorem molestiae ut voluptate.",
+    "title": "Et quis explicabo quas necessitatibus laboriosam a."
+  },
+  {
+    "fileName": "kamille-02.jpg",
+    "name": "Earum nulla eos laborum sunt.",
+    "title": "Eligendi consequuntur autem nemo sed nesciunt delectus."
+  },
+  {
+    "fileName": "lilie-2.jpg",
+    "name": "Vel ducimus explicabo vitae dolorem recusandae.",
+    "title": "Voluptas nobis omnis expedita aut quia eos velit."
+  },
+  {
+    "fileName": "lilie-3.jpg",
+    "name": "Aliquid deleniti modi fugiat eos nulla quibusdam.",
+    "title": "Temporibus qui explicabo qui voluptas omnis nihil et repellat."
+  },
+  {
+    "fileName": "lilie-by-izyliaz.jpg",
+    "name": "Deserunt aperiam natus soluta qui.",
+    "title": "Impedit non sapiente accusamus minus."
+  },
+  {
+    "fileName": "lilie-by-schnitzel-of-pain.jpg",
+    "name": "Ut maiores voluptas illum vitae animi iure.",
+    "title": "Dolorem placeat totam sed qui."
+  },
+  {
+    "fileName": "lilie.jpg",
+    "name": "Labore qui quae.",
+    "title": "Quo dolorem eius beatae quia et recusandae eveniet."
+  },
+  {
+    "fileName": "lilli-by-creation-victim.jpg",
+    "name": "Sint cumque ut voluptatibus ut sequi error.",
+    "title": "Maiores at fuga quaerat modi nemo nisi consectetur eos."
+  },
+  {
+    "fileName": "loewenzahn-01.jpg",
+    "name": "Adipisci consequatur aut illum doloremque eum distinctio.",
+    "title": "Placeat fuga accusantium aut unde nihil incidunt et."
+  },
+  {
+    "fileName": "loewenzahn-02.jpg",
+    "name": "Minus enim voluptatibus qui pariatur tempora.",
+    "title": "Minus ex tempore perferendis labore veritatis."
+  },
+  {
+    "fileName": "loewenzahn-03.jpg",
+    "name": "Et cum labore pariatur.",
+    "title": "Voluptatem aut voluptatum aliquid fugiat alias."
+  },
+  {
+    "fileName": "loewenzahn-04.jpg",
+    "name": "Dolore dicta cupiditate provident asperiores laboriosam.",
+    "title": "Non perferendis explicabo id consequatur nulla incidunt aspernatur velit."
+  },
+  {
+    "fileName": "loewenzahn-05.jpg",
+    "name": "Fuga maxime doloremque.",
+    "title": "Omnis ut deleniti molestias."
+  },
+  {
+    "fileName": "loewenzahn-06.jpg",
+    "name": "Similique sed in culpa.",
+    "title": "Incidunt dolorem quaerat laudantium natus dolores ut."
+  },
+  {
+    "fileName": "loewenzahn-07.jpg",
+    "name": "Soluta quibusdam architecto itaque.",
+    "title": "Sed eaque ex nobis consequuntur aspernatur ut cumque."
+  },
+  {
+    "fileName": "loewenzahn-08.png",
+    "name": "Cum est in sint explicabo.",
+    "title": "Sunt sit et ut voluptates."
+  },
+  {
+    "fileName": "loewenzahn-09.png",
+    "name": "Harum perferendis quis quis qui veniam aperiam.",
+    "title": "Aliquid dolores sunt ea."
+  },
+  {
+    "fileName": "mohn-01.jpg",
+    "name": "Consequatur asperiores.",
+    "title": "Et magnam laborum corporis saepe."
+  },
+  {
+    "fileName": "mohn-02.jpg",
+    "name": "Nisi corporis quia.",
+    "title": "Error consectetur natus consequatur tempore tenetur consequatur."
+  },
+  {
+    "fileName": "mohn-03.jpg",
+    "name": "Autem eveniet.",
+    "title": "Exercitationem qui qui tempora."
+  },
+  {
+    "fileName": "mohn-04.jpg",
+    "name": "Ad eius voluptatem vitae voluptatem.",
+    "title": "Quia qui doloremque est necessitatibus consequatur asperiores."
+  },
+  {
+    "fileName": "mohn-05.jpg",
+    "name": "Dicta doloremque itaque consectetur delectus quia ea.",
+    "title": "Odit velit aperiam veritatis."
+  },
+  {
+    "fileName": "old-house-59.4.jpg",
+    "name": "Et culpa esse vel minima nam deleniti.",
+    "title": "Quis maiores libero necessitatibus."
+  },
+  {
+    "fileName": "phalaenopsis-orchids-moth-orchids.jpg",
+    "name": "Qui ab aut facilis.",
+    "title": "Quaerat vel minus et omnis nostrum tenetur sapiente omnis."
+  },
+  {
+    "fileName": "phoenix-1440x900.jpg",
+    "name": "Ea sit nostrum aliquam unde possimus consectetur.",
+    "title": "Laudantium quia non consequatur minus."
+  },
+  {
+    "fileName": "sb10066927aj-001.jpg",
+    "name": "Aut corrupti nesciunt beatae minima et.",
+    "title": "Aut architecto quas aliquid tenetur praesentium cumque est."
+  },
+  {
+    "fileName": "snowdrop,-the-first-spring-flower.jpg",
+    "name": "Cum quam.",
+    "title": "Labore vitae facilis et."
+  },
+  {
+    "fileName": "strawberry-01.jpg",
+    "name": "Aliquam placeat consequatur et nulla ducimus minus.",
+    "title": "Ad aut sint optio soluta tempore sunt reiciendis earum."
+  },
+  {
+    "fileName": "strawberry-02.jpg",
+    "name": "Tenetur similique inventore.",
+    "title": "Neque aut quis nemo."
+  },
+  {
+    "fileName": "vista-15-1280x1024.jpg",
+    "name": "Consectetur voluptas architecto.",
+    "title": "Tenetur nostrum debitis perferendis sint ratione officia eos."
+  },
+  {
+    "fileName": "yachtcharter-thailand-james-bond.jpg",
+    "name": "Eos rerum quam dolor voluptatem facilis voluptas.",
+    "title": "Vero eius enim atque reiciendis est tempora assumenda nostrum."
   }
-];
+].map(entry => {
+  entry.url = '/build/' + require('../images/photos_gallery/' + entry.fileName);
+  return entry;
+});
+
+console.log(photos);
 
 export function getPhotos(){ return photos; }
 export function getPhoto(id){ return photos[id]; }
