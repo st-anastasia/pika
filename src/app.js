0774d5545ef9e00c526a803e91fe17a41b1ce094
stan <anastasia.stieb@gmail.com>
2016-04-25 13:42:23 +0200

    photo-detail style

--- src/app.js
+++ src/app.js
@@ -25,15 +25,17 @@ angular.module("pika", ['ngRoute', 'ngMaterial'])
       controller: "PhotoDetailController as controller"
     });
     $mdThemingProvider.theme('default')
-    .primaryPalette('blue-grey', {
-      'default': '400',
-      'hue-1': '50',
-      'hue-2': '800',
-    })
-    .backgroundPalette('blue-grey', {
-      'default': '50',
-      'hue-1': '800'
-    })
-    .accentPalette('cyan');
+      .primaryPalette('blue-grey', {
+        'default': '400',
+        'hue-1': '50',
+        'hue-2': '800',
+      })
+      .backgroundPalette('blue-grey', {
+        'default': '50',
+        'hue-1': '800',
+        'hue-2': '900'
+      })
+      .accentPalette('cyan')
+      .dark();
  });
 
