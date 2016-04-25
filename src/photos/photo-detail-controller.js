ae5a3b7f8f50d281f33d9669b7f5a3ebc8c27cde
stan <anastasia.stieb@gmail.com>
2016-04-25 13:44:12 +0200

    toolbar menu

--- src/photos/photo-detail-controller.js
+++ src/photos/photo-detail-controller.js
@@ -3,8 +3,28 @@
 import { getPhoto } from "../data/database";
 
 class PhotoDetailController{
-  constructor($routeParams){
+  constructor($routeParams, $mdDialog){
     this.photo = getPhoto($routeParams.photoId);
+
+    this.originatorEvent = null;
+    this.$mdDialog = $mdDialog;
+  }
+
+  openMenu($mdOpenMenu, event){
+    this.originatorEvent = event;
+    $mdOpenMenu(event);
+  }
+
+  menuItemClick(index){
+    this.$mdDialog.show(
+      this.$mdDialog.alert()
+        .title('You clicked!')
+        .textContent('Menu Item clicked, index: ' + index)
+        .ok('OK')
+        .targetEvent(this.originatorEvent)
+    );
+
+    this.originatorEvent = null;
   }
 }
 
