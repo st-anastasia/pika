3080bc389f873ca49b02505b121d843cf4c716a7
Anastasia Stieb <anastasia.stieb@gmail.com>
2017-05-07 18:50:49 +0200

    eslint

--- client/app/photos/service.js
+++ client/app/photos/service.js
@@ -26,105 +26,102 @@ class PhotosService {
     const self = this;
     if (this.currentPhoto.id === id) return;
 
-    const foundIndex = this.photos.findIndex(photo => photo._id === id );
-    if(foundIndex >= 0 ) {
-      this._slideTo(foundIndex);
+    // eslint-disable-next-line no-underscore-dangle
+    const foundIndex = this.photos.findIndex(photo => photo._id === id);
+    if (foundIndex >= 0) {
+      this.slideTo(foundIndex);
       return;
     }
 
-    this.$http.get(`/api/photos/${id}`).then(({data}) => {
-      self._currentPhoto = data;
-      self._setSliding({state: 'off'});
+    this.$http.get(`/api/photos/${id}`).then(({ photo }) => {
+      self.currentPhoto = photo;
+      self.setSliding({ state: 'off' });
 
       return photo;
     });
   }
 
-  loadPhotos(params={}){
-    const {page=1, limit=LIMIT, search=this.search} = params;
+  loadPhotos(params = {}) {
+    const { page = 1, limit = LIMIT, search = this.search } = params;
     const self = this;
 
-    return this.$http.get('/api/photos', {params: {page, limit, search}})
-      .then( res => {
-        const {data: {photos, totalSize}} = res;
+    return this.$http.get('/api/photos', { params: { page, limit, search } })
+      .then((res) => {
+        const { data: { photos, totalSize } } = res;
         self.search = search;
         self.currentPage = page;
         self.photos = photos;
         self.totalSize = totalSize;
-        self._paginate();
+        self.paginate();
         return photos;
-    });
+      });
   }
 
-  prev(step = 1){
+  prev(step = 1) {
     const self = this;
-    if (this._slideTo(this.currentIndex - step) !== null) {
+    if (this.slideTo(this.currentIndex - step) !== null) {
       return Promise.resolve(this.currentPhoto);
     }
 
     const prevPage = this.currentPage - 1;
-    if(prevPage < 0) {
+    if (prevPage < 0) {
       return Promise.resolve(this.currentPhoto);
     }
 
-    return this.loadPhotos({page: prevPage}).then( photos => {
-      return self._slideTo(photos.length - 1);
-    });
+    return this.loadPhotos({ page: prevPage }).then(photos => self.slideTo(photos.length - 1));
   }
 
-  next(step = 1){
+  next(step = 1) {
     const self = this;
-    if (this._slideTo(this.currentIndex + step) !== null) {
+    if (this.slideTo(this.currentIndex + step) !== null) {
       return Promise.resolve(this.currentPhoto);
     }
 
     const nextPage = this.currentPage + 1;
-    return this.loadPhotos({page: nextPage}).then( photos => {
-      return self._slideTo(0);
-    });
+    return this.loadPhotos({ page: nextPage }).then(photos => self.slideTo(0));
   }
 
-  _paginate(){
+  paginate() {
     let start = this.currentPage - 2;
     if (start < 1) start = 1;
 
     let end = start + 5;
-    if (this.totalSize / LIMIT < 5 ) {
-      start =   1;
-      end   =   Math.ceil(this.totalSize / LIMIT ) + 1;
+    if (this.totalSize / LIMIT < 5) {
+      start = 1;
+      end = Math.ceil(this.totalSize / LIMIT) + 1;
     }
 
     this.pages = _.range(start, end);
   }
 
-  _slideTo(index){
-    if (this._setCurrentIndex(index) === null) return null;
+  slideTo(index) {
+    if (this.setCurrentIndex(index) === null) return null;
 
-    this._setSliding();
+    this.setSliding();
     return this.currentPhoto;
   }
 
-  _setCurrentIndex(index){
-    if(index < 0 || index > this.photos.length - 1) return null;
+  setCurrentIndex(index) {
+    if (index < 0 || index > this.photos.length - 1) return null;
 
     this.currentPhoto = this.photos[index];
     this.currentIndex = index;
     return this.currentIndex;
   }
 
-  _setSliding({state}={}){
+  setSliding({ state } = {}) {
     if (state === 'off') {
-      return this.sliding = {next: false, prev: false};
+      return this.sliding = { next: false, prev: false };
     }
 
     this.sliding = {
       prev: this._offset() !== 0,
-      next: this._offset() + 1 !== this.totalSize
+      next: this._offset() + 1 !== this.totalSize,
     };
     return this.sliding;
   }
 
-  _offset(){
+  _offset() {
     return (this.currentPage - 1) * LIMIT + this.currentIndex;
   }
 }
