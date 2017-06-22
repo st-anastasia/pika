c00ed69c7aa055d18bfb711385c0954e8577c832
Anastasia Stieb <anastasia.stieb@gmail.com>
2017-06-22 10:23:08 +0200

    first draft

--- client/app/photos/toolbar-component.js
+++ client/app/photos/toolbar-component.js
@@ -2,13 +2,16 @@ import template from './toolbar.jade';
 
 class ToolbarController {
   /** @ngInject */
-  constructor($state, $mdSidenav, $location, photosService) {
+  constructor($state, $mdSidenav, $location, photosService, Upload) {
+    this.$state = $state;
     this.$mdSidenav = $mdSidenav;
     this.$location = $location;
-    this.$state = $state;
-
     this.photosService = photosService;
+    this.Upload = Upload;
+
     this.searchTerm = '';
+    this.uploadProgress = 0;
+    this.errorMsg = null;
   }
 
   toggleSideMenu() {
@@ -18,6 +21,27 @@ class ToolbarController {
   search() {
     this.photosService.loadPhotos({ search: this.searchTerm });
     this.$state.go('photos', { search: this.searchTerm }, { location: 'replace', notify: false });
+  }
+
+  uploadPhotos(files) {
+    const self = this;
+    const uploadCounter = 0;
+
+    files.forEach((file) => {
+      Upload.upload({
+        url: '/api/photos',
+        data: {file: file}
+      }).then((response) => {
+        uploadCounter++
+        if (uploadCounter === files.size) console.log('ready');
+      }, (response) => {
+        if (response.status > 0) {
+          self.errorMsg = response.status + ': ' + response.data;
+        }
+      }, (evt) => {
+        self.uploadProgress = Math.min(100, parseInt(100.0 * evt.loaded / files.size));
+      });
+    });
   }
 }
 
